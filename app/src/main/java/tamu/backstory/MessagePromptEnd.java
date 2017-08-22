7a070106e35671f82320961a211afe9438313e3f
Colin Banigan <colinbanigan@yahoo.com>
2017-08-21 22:58:25 -0500

    initializing

---
+++ app/src/main/java/tamu/backstory/MessagePromptEnd.java
@@ -0,0 +1,89 @@
+package tamu.backstory;
+
+import android.app.ActivityOptions;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/*
+    The end screen after the recording is completed - contains the main screen, badge screen, and confirmation
+ */
+
+public class MessagePromptEnd extends FragmentActivity implements
+        MessagePromptEndFragment.OnFragmentInteractionListener,
+        MessagePromptEndBadgeFragment.OnFragmentInteractionListener,
+        MessagePromptEndConfirmationFragment.OnFragmentInteractionListener {
+
+    private TextView mTextView;
+    public static int missionNumber;
+    public static Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_prompt_end);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+        mContext = getApplicationContext();
+        loadMissionNumber();
+        setContentView(R.layout.rect_activity_message_prompt_end);
+        setBackground();
+        ViewPager myVP = (ViewPager) this.findViewById(R.id.pager);
+        CircleIndicator indicator = (CircleIndicator) this.findViewById(R.id.indicator);
+        myVP.setAdapter(new MessagePromptEndFragAdapter(getSupportFragmentManager()));
+        indicator.setViewPager(myVP);
+    }
+
+    public void loadMissionNumber() {
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        if(bundle != null){
+            missionNumber = bundle.getInt("missionNumber");
+        }
+    }
+
+    public void setBackground(){
+        ImageView background = (ImageView) this.findViewById(R.id.messagePromptEndBackground);
+        System.out.println("missionNumber message prompt: " + missionNumber);
+        //#offBy1Error, fixed
+        //completely temporary, don't do this at home, kids
+        if(missionNumber == 0) background.setBackground(getResources().getDrawable(R.drawable.gameified_backstory_story_background_1));
+        if(missionNumber == 1) background.setBackground(getResources().getDrawable(R.drawable.gameified_backstory_story_background_2));
+        if(missionNumber == 2) background.setBackground(getResources().getDrawable(R.drawable.gameified_backstory_story_background_3));
+    }
+
+    public void returnToMenu(View v){
+        ChapterDataSingleton.getSync(this).updateListMissionData();
+        finish();
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+    }
+
+    public void returnToRecording(View v){
+        Intent recording = new Intent(this, MessageRecord.class);
+        ActivityOptions options = ActivityOptions.makeScaleUpAnimation(v, 0,
+                0, v.getWidth(), v.getHeight());
+        startActivity(recording, options.toBundle());
+        finish();
+    }
+
+    public void onFragmentInteraction(Uri uri){
+
+    }
+
+    public void onFragmentInteractionConfirmation(Uri uri){
+
+    }
+}
---
+++ app/src/main/java/tamu/backstory/MessagePromptEndFragAdapter.java
@@ -0,0 +1,41 @@
+package tamu.backstory;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by colin on 9/10/2016.
+ */
+public class MessagePromptEndFragAdapter extends FragmentPagerAdapter {
+
+    public MessagePromptEndFragAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        if(ChapterDataSingleton.getSync(NewMainMenu.mContext).getNumberOfRecordings(MessagePromptEnd.missionNumber) > 2){
+            return 3;
+        }
+        return 2;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        if(ChapterDataSingleton.getSync(MessagePromptEnd.mContext).getNumberOfRecordings(MessagePromptEnd.missionNumber) > 2){
+            switch(i) {
+                case 0: return MessagePromptEndFragment.newInstance("", "");
+                case 1: return MessagePromptEndBadgeFragment.newInstance("", "");
+                default: return MessagePromptEndConfirmationFragment.newInstance("", "");
+            }
+        } else {
+            switch(i) {
+                case 0: return MessagePromptEndFragment.newInstance("", "");
+                default: return MessagePromptEndConfirmationFragment.newInstance("", "");
+            }
+        }
+    }
+}
